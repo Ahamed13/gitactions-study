04fe69ce68d1e3f5f7aa496f34f20ca6803b24f6
ahamed13 <ahamed13041998@gmail.com>
2024-10-16 13:59:27 +0530

    updated code

--- variables.tf
+++ variables.tf
@@ -7,3 +7,11 @@ variable "location" {
     type = string
     default = "East US"
 }
+
+variable "subscription_id" {}
+
+variable "tenant_id" {}
+
+variable "client_id" {}
+
+variable "client_secret" {}
