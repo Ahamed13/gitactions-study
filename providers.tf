822f410477c342981f53aad2332a25b6ab846743
ahamed13 <ahamed13041998@gmail.com>
2024-10-16 13:21:50 +0530

    added provider

---
+++ providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=4.5.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    // subscription_id = var.subscription_id
+    // tenant_id = var.tenant_id
+    // client_id = var.client_id
+    // client_secret = var.client_secret
+  features {}
+}
